e22f14cc8394e47483ef80af68ea5808094a340b
MussieTeka <93384473+MussieTeka@users.noreply.github.com>
2023-03-10 11:07:00 +0300

    Add updated JS file to make create form validaton.

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function openMenu() {
 function closeMenu() {
   sidemenu.style.right = "-768px";
 }
+
+//Form Validation
+
+const form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const emailInput = document.querySelector("#email");
+  const email = emailInput.value;
+
+  if (email === email.toLowerCase()) {
+    // email is in lowercase, form can be submitted
+    form.submit();
+  } else {
+    // email is not in lowercase, show error message
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent =
+      "Sorry! Error occured. Email must be in lowercase!";
+    form.appendChild(errorMessage);
+  }
+}
